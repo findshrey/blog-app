4a7b5aafc0612a99dd2a6b6befcdcb53d98a03b3
shrey9496 <shrey9april@gmail.com>
2021-03-07 23:54:33 +0530

    Feat: Add JSON server for prototyping.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import BlogList from "./BlogList"
 
 const Home = () => {
-   const [blogs, setBlogs] = useState([
-      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-   ])
+   const [blogs, setBlogs] = useState([])
+
+   useEffect(() => {
+      fetch('http://localhost:8000/blogs')
+         .then(res => {
+            return res.json()
+         })
+         .then(data => {
+            setBlogs(data)
+         })
+   }, [])
 
    return (
       <div className="home">
          <BlogList blogs={blogs} title="All Blogs" />
-         <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Mario's Blogs" />
       </div>
    )
 }
